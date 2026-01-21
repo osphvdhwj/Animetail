f69673edb3066c84c84ad2245671677652a86cc0
osphvdhwj <150425690+osphvdhwj@users.noreply.github.com>
2026-01-21 14:28:00 +0530

    Update GestureHandler.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/player/controls/GestureHandler.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/player/controls/GestureHandler.kt
@@ -22,7 +22,6 @@ import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
